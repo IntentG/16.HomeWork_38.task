aa8151b7515c201a4838821d618e81966b739c37
IntentG <Sublustrum123@gmail.com>
2022-05-30 21:25:12 +0600

    Задача 38.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите
+разницу между максимальным и минимальным
+элементов массива.   [3 7 22 2 78] -> 76   */
+
+Console.WriteLine("Укажите длину массива: ");
+int sizeArray = int.Parse(Console.ReadLine());
+
+int[] array = new int[sizeArray];
+Random arrayRandomValue = new Random();
+int temp = 0;
+int differenceArrayValue = 0;
+
+///Создание массива с указанной длиной:
+Console.WriteLine(" Изначально несортированный массив:");
+for (int i = 0; i < sizeArray; i++)
+{
+    array[i] = arrayRandomValue.Next(-100, 100);
+    Console.Write($" | {array[i]}");
+}
+
+///Сортировка массива:
+Console.WriteLine("\n \n Отсортированный массив массив:");
+for (int i = 0; i < array.Length; i++)
+{
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (array[i] > array[j])
+        {
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+    Console.Write($" | {array[i]}");
+}
+
+differenceArrayValue = array[array.Length - 1] - array[0];
+
+Console.WriteLine($"\n \n Разница между максимальным и минимальным элементом массива: ");
+Console.WriteLine($" ({array[array.Length - 1]}) - ({array[0]}) = {differenceArrayValue}"); 
